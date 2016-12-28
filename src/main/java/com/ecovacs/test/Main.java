5c9acd0f9d58739ad2026cb5c2d9e873ea6bef39
lily.shan <shandan.flora@gmail.com>
2016-12-28 16:28:06 +0800

    commit for adding column of server in excel

--- src/main/java/com/ecovacs/test/Main.java
+++ src/main/java/com/ecovacs/test/Main.java
@@ -1,9 +1,6 @@
 package com.ecovacs.test;
 
-import com.ecovacs.test.common.ExcelManage;
-import com.ecovacs.test.common.ExcelRow;
-import com.ecovacs.test.common.JsonParse;
-import com.ecovacs.test.common.PropertyData;
+import com.ecovacs.test.common.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,46 +15,49 @@ public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String args[]){
-        HandleIntl.getInstance().initAppium();
         List<String> list = JsonParse.getJsonParse().readDataFromJson("country.json", "countries");
+        HandleIntl.getInstance().initAppium();
         if (!HandleIntl.getInstance().enterWelcomeActivity()){
             return;
         }
         int iLoop = 1;
         ExcelManage.getInstance().init();
         for(String strCountry:list) {
-            logger.info("***********Ready register country-- " + strCountry + "***********");
+            //logger.info("***********Ready register country-- " + strCountry + "***********");
             ExcelRow row = new ExcelRow();
             row.setOrdinal(iLoop);
-            /*row.setType("Register");
+            row.setType("Register");
             row.setCountry(strCountry);
             if(!HandleIntl.getInstance().registerAndLogin(strCountry,
                     PropertyData.getProperty("email_type"),
                     PropertyData.getProperty("gmail_email"),
                     PropertyData.getProperty("register_pass"))){
                 row.setResult("Fail");
-                ExcelManage.getInstance().writeRow(iLoop, row, false);
+                ExcelManage.getInstance().writeRow(iLoop, row, false, Common.getInstance().getFailType());
                 logger.error("***********Register country--" + strCountry + " failed!!!***********");
+                iLoop++;
                 continue;
             }
             row.setResult("Pass");
-            ExcelManage.getInstance().writeRow(iLoop, row, true);
+            ExcelManage.getInstance().writeRow(iLoop, row, true, Common.getInstance().getFailType());
             logger.info("***********Register country -" + strCountry + " successfully!!!***********");
-            logger.info("***********Ready to Forget password country-" + strCountry + "***********");*/
+            logger.info("***********Ready to Forget password country-" + strCountry + "***********");
             row.setType("ForgetPassword");
             if(!HandleIntl.getInstance().forgetPassword(strCountry,
                     PropertyData.getProperty("email_type"),
                     PropertyData.getProperty("gmail_email"),
                     PropertyData.getProperty("login_pass"))){
                 row.setResult("Fail");
-                ExcelManage.getInstance().writeRow(iLoop, row, false);
+                ExcelManage.getInstance().writeRow(iLoop, row, false, Common.getInstance().getFailType());
                 logger.error("***********Forget password country--" + strCountry + " failed!!!***********");
+                iLoop++;
                 continue;
             }
             row.setResult("Pass");
-            ExcelManage.getInstance().writeRow(iLoop, row, true);
+            ExcelManage.getInstance().writeRow(iLoop, row, true, Common.getInstance().getFailType());
             logger.info("***********Forget password country -" + strCountry + " successfully!!!***********");
             iLoop++;
         }
+        ExcelManage.getInstance().writeColServer();
     }
 }
--- src/main/java/com/ecovacs/test/common/ExcelManage.java
+++ src/main/java/com/ecovacs/test/common/ExcelManage.java
@@ -5,11 +5,13 @@ import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCreationHelper;
 import org.apache.poi.xssf.usermodel.XSSFHyperlink;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.List;
 
 /**
  * Created by ecosqa on 16/12/17.
@@ -53,6 +55,7 @@ public class ExcelManage {
         sheet.setColumnWidth((short) 4, (short) 4000);
         sheet.setColumnWidth((short) 5, (short) 4000);
         sheet.setColumnWidth((short) 6, (short) 4000);
+        sheet.setColumnWidth((short) 7, (short) 3000);
         // Row/Cell from 0
         Row row = sheet.createRow((short) 0);
         // Or do it on one line.
@@ -63,6 +66,7 @@ public class ExcelManage {
         row.createCell(4).setCellValue("UserAgreement");
         row.createCell(5).setCellValue("Register");
         row.createCell(6).setCellValue("ForgetPassword");
+        row.createCell(7).setCellValue("Server");
 
         //save
         saveExcel(wb);
@@ -92,8 +96,38 @@ public class ExcelManage {
         }
     }
 
+    public void writeColServer(){
+        try {
+            List<String> listOthers = JsonParse.getJsonParse().readDataFromJson("serverCountry.json", "others");
+            InputStream stream = new FileInputStream(getCurPath() + "TestReport.xlsx");
+            XSSFWorkbook workBook = new XSSFWorkbook(stream);
 
-    public void writeRow(int iRow, ExcelRow excelRow, boolean bPass){
+            CellStyle style = workBook.createCellStyle();
+            style.setFillForegroundColor(HSSFColor.LEMON_CHIFFON.index);
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            // first sheet
+            XSSFSheet sheet = workBook.getSheetAt(0);
+            int iRows = sheet.getLastRowNum();
+
+            for(int i = 1; i <= iRows; i++){
+                Row row = sheet.getRow(i);
+                String strValue = row.getCell(2).getStringCellValue();
+                for(String strCountry: listOthers){
+                    if (strValue.equals(strCountry)){
+                        row.createCell(7).setCellStyle(style);
+                    }
+                }
+            }
+            //save excel
+            saveExcel(workBook);
+            workBook.close();
+            stream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeRow(int iRow, ExcelRow excelRow, boolean bPass, Common.FailType type){
 
         try {
             InputStream stream = new FileInputStream(getCurPath() + "TestReport.xlsx");
@@ -115,6 +149,7 @@ public class ExcelManage {
             row.createCell(2).setCellValue(excelRow.getCountry());
             row.createCell(3).setCellValue(excelRow.getResult());
             row.getCell(3).setCellStyle(style);
+
             //set link style
             CellStyle hlink_style = workBook.createCellStyle();
             Font hlink_font = workBook.createFont();
@@ -123,24 +158,30 @@ public class ExcelManage {
             hlink_style.setFont(hlink_font);
             //set link address
             XSSFCreationHelper helper = new XSSFCreationHelper(workBook);
-            XSSFHyperlink fileLink4 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
-            XSSFHyperlink fileLink5 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
-            XSSFHyperlink fileLink6 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
-            //user agreement.png
-            row.createCell(4).setCellValue("click for view");
-            fileLink4.setAddress("screenShots/UserAgree_" + excelRow.getCountry() + ".png");
-            row.getCell(4).setHyperlink(fileLink4);
-            row.getCell(4).setCellStyle(hlink_style);
-            //register.png
-            row.createCell(5).setCellValue("click for view");
-            fileLink5.setAddress("screenShots/doResetPass-" + excelRow.getCountry() + ".png");
-            row.getCell(5).setHyperlink(fileLink5);
-            row.getCell(5).setCellStyle(hlink_style);
-            //reset password.png
-            row.createCell(6).setCellValue("click for view");
-            fileLink6.setAddress("screenShots/Register-" + excelRow.getCountry() + ".png");
-            row.getCell(6).setHyperlink(fileLink6);
-            row.getCell(6).setCellStyle(hlink_style);
+
+            if(type != Common.FailType.ALREADY_REGISTER && excelRow.getType().equals("Register")){
+                XSSFHyperlink fileLink4 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
+                XSSFHyperlink fileLink5 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
+                //user agreement.png
+                row.createCell(4).setCellValue("click for view");
+                fileLink4.setAddress("screenShots/UserAgree_" + excelRow.getCountry() + ".png");
+                row.getCell(4).setHyperlink(fileLink4);
+                row.getCell(4).setCellStyle(hlink_style);
+                //register.png
+                row.createCell(5).setCellValue("click for view");
+                fileLink5.setAddress("screenShots/Register-" + excelRow.getCountry() + ".png");
+                row.getCell(5).setHyperlink(fileLink5);
+                row.getCell(5).setCellStyle(hlink_style);
+            }
+
+            if(type != Common.FailType.NOT_REGISTER && excelRow.getType().equals("ForgetPassword")){
+                XSSFHyperlink fileLink6 = new XSSFHyperlink(helper.createHyperlink(HyperlinkType.FILE));
+                //reset password.png
+                row.createCell(6).setCellValue("click for view");
+                fileLink6.setAddress("screenShots/doResetPass-" + excelRow.getCountry() + ".png");
+                row.getCell(6).setHyperlink(fileLink6);
+                row.getCell(6).setCellStyle(hlink_style);
+            }
             //save excel
             saveExcel(workBook);
             workBook.close();
